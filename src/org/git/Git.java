8ad28d1465fc7550891f8c125812c581c4254531
praveenspec94-collab <praveen.spec94@gmail.com>
2025-08-28 15:45:18 +0530

    New commit

--- src/org/git/Git.java
+++ src/org/git/Git.java
@@ -6,11 +6,16 @@ public class Git {
 		System.out.println("Git Hub");
 
 	}
+	private void method2() {
+System.out.println("Git update");
+	}
 	
 	public static void main(String[] args) {
 		
 		Git a = new Git();
 		a.method1();
+		a.method2();
+		
 	}
 
 }
